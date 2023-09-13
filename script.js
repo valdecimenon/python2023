334e5c02103a953499831a84fa81ed1179465d90
Valdeci Menon <valdecimenon71@yahoo.com.br>
2023-09-13 19:48:01 -0300

    conteudo web

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function mudaVermelho() {
+    const botao = document.getElementById('red')
+    botao.style.color = 'red'
+}
+
+function mudaVerde() {
+    const botao = document.getElementById('green')
+    botao.style.color = 'green'
+}
+
+function mudaAzul() {
+    const botao = document.getElementById('blue')
+    botao.style.color = 'blue'
+}
+
+function mudaLaranja() {
+    const botao = document.getElementById('orange')
+    botao.style.color = 'orange'
+}
